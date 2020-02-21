8256ef69ce4aa7bba94e5f1bc221a2632d625bda
aayush.bhargava <aayush.bhargava@jungleworks.com>
2020-02-21 17:45:23 +0530

    Fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable, Subject, ReplaySubject} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map, takeUntil} from 'rxjs/operators';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, Subject, ReplaySubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, takeUntil, take } from 'rxjs/operators';
 import { ApiHitService } from '../apiHit.service';
 import { Router } from '@angular/router';
 import { NgProgress } from 'ngx-progressbar';
 import { FormControl } from '@angular/forms';
-
+import { MatSelect } from '@angular/material';
 interface Bank {
   id: string;
   name: string;
- }
+}
 
 @Component({
   selector: 'app-home',
@@ -19,44 +19,60 @@ interface Bank {
 export class HomeComponent implements OnInit {
 
 
-  private _onDestroy = new Subject<void>();
-  public filteredBanksMulti: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
-
+  @ViewChild('multiSelect') multiSelect: MatSelect;
   public cityArray = [];
   public cityName: string = '';
   searchCityArray: any = [];
+  /** control for the selected bank for multi-selection */
   public bankMultiCtrl: FormControl = new FormControl();
+
+  /** control for the MatSelect filter keyword multi-selection */
   public bankMultiFilterCtrl: FormControl = new FormControl();
-  public banks = [
-    {name: 'Bank A (Switzerland)', id: 'A'},
-    {name: 'Bank B (Switzerland)', id: 'B'},
-    {name: 'Bank C (France)', id: 'C'},
-    {name: 'Bank D (France)', id: 'D'},
-    {name: 'Bank E (France)', id: 'E'},
-    {name: 'Bank F (Italy)', id: 'F'},
-    {name: 'Bank G (Italy)', id: 'G'},
-    {name: 'Bank H (Italy)', id: 'H'},
-    {name: 'Bank I (Italy)', id: 'I'},
-    {name: 'Bank J (Italy)', id: 'J'},
-    {name: 'Bank K (Italy)', id: 'K'},
-    {name: 'Bank L (Germany)', id: 'L'},
-    {name: 'Bank M (Germany)', id: 'M'},
-    {name: 'Bank N (Germany)', id: 'N'},
-    {name: 'Bank O (Germany)', id: 'O'},
-    {name: 'Bank P (Germany)', id: 'P'},
-    {name: 'Bank Q (Germany)', id: 'Q'},
-    {name: 'Bank R (Germany)', id: 'R'} 
+
+  /** list of banks filtered by search keyword */
+  public filteredBanksMulti: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
+
+  /** Subject that emits when the component has been destroyed. */
+  protected _onDestroy = new Subject<void>();
+
+  public banks: Bank[] = [
+    { name: 'Bank A (Switzerland)', id: 'A' },
+    { name: 'Bank B (Switzerland)', id: 'B' },
+    { name: 'Bank C (France)', id: 'C' },
+    { name: 'Bank D (France)', id: 'D' },
+    { name: 'Bank E (France)', id: 'E' },
+    { name: 'Bank F (Italy)', id: 'F' },
+    { name: 'Bank G (Italy)', id: 'G' },
+    { name: 'Bank H (Italy)', id: 'H' },
+    { name: 'Bank I (Italy)', id: 'I' },
+    { name: 'Bank J (Italy)', id: 'J' },
+    { name: 'Bank K (Italy)', id: 'K' },
+    { name: 'Bank L (Germany)', id: 'L' },
+    { name: 'Bank M (Germany)', id: 'M' },
+    { name: 'Bank N (Germany)', id: 'N' },
+    { name: 'Bank O (Germany)', id: 'O' },
+    { name: 'Bank P (Germany)', id: 'P' },
+    { name: 'Bank Q (Germany)', id: 'Q' },
+    { name: 'Bank R (Germany)', id: 'R' }
   ]
 
-  constructor(private hit : ApiHitService,private router: Router,public ngProgress: NgProgress) { 
+  constructor(private hit: ApiHitService, private router: Router, public ngProgress: NgProgress) {
   }
 
   ngOnInit() {
+
+    this.bankMultiCtrl.setValue([this.banks[10], this.banks[11], this.banks[12]]);
+
+    // load the initial bank list
+    this.filteredBanksMulti.next(this.banks.slice());
+
+    // listen for search field value changes
     this.bankMultiFilterCtrl.valueChanges
       .pipe(takeUntil(this._onDestroy))
-      .subscribe(() => {        
+      .subscribe(() => {
         this.filterBanksMulti();
       });
+
     this.cityArray = [
       {
         "entity_type": "city",
@@ -113,17 +129,16 @@ export class HomeComponent implements OnInit {
         "country_id": 1,
         "country_name": "India"
       },
-  ]
+    ]
   }
 
-  private filterBanksMulti() {
-    console.log('in func');
-    
+  protected filterBanksMulti() {
     if (!this.banks) {
       return;
     }
     // get the search keyword
     let search = this.bankMultiFilterCtrl.value;
+    
     if (!search) {
       this.filteredBanksMulti.next(this.banks.slice());
       return;
@@ -136,8 +151,6 @@ export class HomeComponent implements OnInit {
     );
   }
 
-
-
   search = (text$: Observable<string>) =>
     text$.pipe(
       debounceTime(200),
@@ -145,65 +158,84 @@ export class HomeComponent implements OnInit {
         : this.searchCityArray.filter(v => v.city_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
     )
 
-  formatter = (x: {city_name: string}) => x.city_name;
-        
+  formatter = (x: { city_name: string }) => x.city_name;
 
-    searchCity(){
-      // this.cityName = document.getElementById('typeahead-template')['value'];
-      if(this.cityName.length > 2){
+
+  searchCity() {
+    // this.cityName = document.getElementById('typeahead-template')['value'];
+    if (this.cityName.length > 2) {
       let data = {
         'cityName': this.cityName
       }
-        this.hit.getCities(data).subscribe((result) => {
-          let data = JSON.parse(result['_body']).location_suggestions;
-          this.searchCityArray = data;
-        })
-      }
-      
-    }
-
-    onSelectTypeahead(data){
-      data.limit = 20;
-      this.searchRestaurants(data);
-    }
-
-    searchRestaurants(data){
-      data.limit = 20;
-      this.ngProgress.start();
-      this.hit.getRestaurants(data).subscribe((result) => {
-        if(result.status == 200){
-          let tmp_data = JSON.parse(result['_body']);
-          tmp_data.limit = 20;
-          tmp_data.latitude = data.latitude;
-          tmp_data.longitude = data.longitude;
-          this.hit.sendRestaurants(tmp_data);
-          this.router.navigate(['/restaurants']);
-          this.ngProgress.done();
-        }
+      this.hit.getCities(data).subscribe((result) => {
+        let data = JSON.parse(result['_body']).location_suggestions;
+        this.searchCityArray = data;
       })
     }
 
-    getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition( position => {
-          let lat = position.coords.latitude;
-          let lng = position.coords.longitude;
-          if(lat && lng){
-            let data = {
-              lat : lat,
-              lng: lng
-            }
-            this.hit.getCities(data).subscribe((result) => {
-              let data = JSON.parse(result['_body']).location_suggestions[0];
-              data.limit = 20;
-              this.searchRestaurants(data);
-            })
-          }
-        })
-      } else { 
-          console.log('error')
-      }
   }
 
+  onSelectTypeahead(data) {
+    data.limit = 20;
+    this.searchRestaurants(data);
+  }
 
+  searchRestaurants(data) {
+    data.limit = 20;
+    this.ngProgress.start();
+    this.hit.getRestaurants(data).subscribe((result) => {
+      if (result.status == 200) {
+        let tmp_data = JSON.parse(result['_body']);
+        tmp_data.limit = 20;
+        tmp_data.latitude = data.latitude;
+        tmp_data.longitude = data.longitude;
+        this.hit.sendRestaurants(tmp_data);
+        this.router.navigate(['/restaurants']);
+        this.ngProgress.done();
+      }
+    })
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        if (lat && lng) {
+          let data = {
+            lat: lat,
+            lng: lng
+          }
+          this.hit.getCities(data).subscribe((result) => {
+            let data = JSON.parse(result['_body']).location_suggestions[0];
+            data.limit = 20;
+            this.searchRestaurants(data);
+          })
+        }
+      })
+    } else {
+      console.log('error')
+    }
+  }
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  protected setInitialValue() {
+    this.filteredBanksMulti
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.multiSelect.compareWith = (a: Bank, b: Bank) => a && b && a.id === b.id;
+      });
+  }
 }
